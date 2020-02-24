5e697fe6638e08d9913bfdca5c069670f03e192c
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-02-24 09:59:22 -0500

    Clase ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+/* const element = document.createElement("h1");
+element.innerText = "Hello, Platzi Badges!";
 
-// const container = document.getElementById('app');
+const container = document.getElementById("app");
 
-// container.appendChild(element);
+container.appendChild(element); */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"; // Análogo al create element
+import ReactDOM from "react-dom"; //Análogo a appendChild
 
 const element = <h1>Hello, Platzi Badges!</h1>;
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
