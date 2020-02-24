51cdd0b0fc9b28cb417bd1db85b8f7aff82e0047
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-02-24 11:09:37 -0500

    Clase de JSX

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-/* const element = document.createElement("h1");
-element.innerText = "Hello, Platzi Badges!";
-
-const container = document.getElementById("app");
-
-container.appendChild(element); */
-
 import React from "react"; // Análogo al create element
 import ReactDOM from "react-dom"; //Análogo a appendChild
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const jsx = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement(
+//   "a",
+//   { href: "https://platzi.com" },
+//   "Ir a Platzi."
+// );
+// const name = "Barrett Rohan";
+// const element = React.createElement("h1", {}, `Hola, soy ${name}`);
+// const jsx = <h1>Hola, soy {name}</h1>;
 
+const jsx = (
+  <div>
+    <h1>Hola, soy Diego</h1>
+    <p>Soy Frontend Developer</p>
+  </div>
+);
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
