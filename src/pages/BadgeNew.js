6dea6673ad658577cac3428accae92f601f4752d
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-02-24 19:08:42 -0500

    Levantamiento del estado

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,24 @@ import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
   render() {
     return (
       <div>
@@ -14,22 +32,25 @@ class BadgeNew extends React.Component {
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="logo" />
         </div>
-
         <div>
           <div className="container">
             <div className="row">
               <div className="col-6">
                 <Badge
-                  firstName="Raymond"
-                  lastName="Berge"
-                  twitter="Travis_McKenzie"
-                  jobTitle="Dynamic Branding Designer"
+                  firstName={this.state.form.firstName}
+                  lastName={this.state.form.lastName}
+                  email={this.state.form.emmail}
                   avatarUrl="https://s3.amazonaws.com/uifaces/faces/twitter/justinrhee/128.jpg"
+                  jobTitle={this.state.form.jobTitle}
+                  twitter={this.state.form.twitter}
                 />
               </div>
 
               <div className="col-6">
-                <BadgeForm />
+                <BadgeForm
+                  onChange={this.handleChange}
+                  formValues={this.state.form}
+                />
               </div>
             </div>
           </div>
