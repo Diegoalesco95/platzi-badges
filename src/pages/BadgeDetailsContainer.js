c2d7134952094e4ee79440af72b358405e42cf08
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-02-26 15:44:28 -0500

    Modales

--- src/pages/BadgeDetailsContainer.js
+++ src/pages/BadgeDetailsContainer.js
@@ -9,7 +9,8 @@ class BadgeDetailsContainer extends React.Component {
   state = {
     loading: true,
     error: null,
-    data: undefined
+    data: undefined,
+    modalIsOpen: false
   };
 
   componentDidMount() {
@@ -27,6 +28,26 @@ class BadgeDetailsContainer extends React.Component {
     }
   };
 
+  handleOpenModal = e => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  handleCloseModal = e => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleDeleteBadge = async e => {
+    this.setState({ loading: true, error: null });
+    try {
+      await api.badges.remove(this.props.match.params.badgeId);
+      this.setState({ loading: false });
+
+      this.props.history.push("/badges");
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+
   render() {
     if (this.state.loading) {
       return <PageLoading />;
@@ -34,7 +55,15 @@ class BadgeDetailsContainer extends React.Component {
     if (this.state.error) {
       return <PageError error={this.state.error} />;
     }
-    return <BadgeDetails badge={this.state.data} />;
+    return (
+      <BadgeDetails
+        onOpenModal={this.handleOpenModal}
+        onCloseModal={this.handleCloseModal}
+        modalIsOpen={this.state.modalIsOpen}
+        onDeleteBadge={this.handleDeleteBadge}
+        badge={this.state.data}
+      />
+    );
   }
 }
 export default BadgeDetailsContainer;
